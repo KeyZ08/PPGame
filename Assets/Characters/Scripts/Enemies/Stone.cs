3bbebd381ea87fcab0de356a30270d6d0fbcac11
KeyZ <mr.kiriazamorozka@yandex.ru>
2022-12-24 20:07:43 +0500

    Тень теперь не получает урона от камня

--- Assets/Characters/Scripts/Enemies/Stone.cs
+++ Assets/Characters/Scripts/Enemies/Stone.cs
@@ -8,4 +8,15 @@ public class Stone : Missile
     {
         return gameObject;
     }
+
+    public override void Attack()
+    {
+        Collider2D[] hitTargets = Physics2D.OverlapCircleAll(Area.position, Area.localScale.x / 2, targetAttackLayers);
+        foreach (var target in hitTargets)
+        {
+            var targetEntity = target.GetComponentInParent<Entity>();
+            if (targetEntity is Shadow) continue;
+            Hit(targetEntity);
+        }
+    }
 }
